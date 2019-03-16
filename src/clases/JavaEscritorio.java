999700c2cd871c3ecc5798f70b76fd39857deee2
D4ITON <daitonmh100@gmail.com>
2019-03-15 23:44:39 -0500

    iniciando proyecto.

---
+++ src/clases/JavaEscritorio.java
@@ -0,0 +1,15 @@
+package clases;
+
+import formularios.frmLogin;
+
+public class JavaEscritorio {
+
+
+    public static void main(String[] args) {
+        // lamada al formulario
+        frmLogin miLogin = new frmLogin();
+        miLogin.setLocationRelativeTo(null);
+        miLogin.setVisible(true);
+    }
+    
+}
